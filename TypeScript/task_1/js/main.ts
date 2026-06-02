d3fcdd1cfef3a224b4e5f5bf1afc90f833e8ed40
Fabiola Burgos <fburgos913@gmail.com>
2026-06-01 23:35:01 -0700

    Add Directors interface

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -4,17 +4,29 @@ interface Teacher {
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
-
   [attribute: string]: any;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
 }
 
 const teacher3: Teacher = {
   firstName: 'John',
   lastName: 'Doe',
-  fullTimeEmployee: false,
+  fullTimeEmployee: true,
   location: 'London',
-  contract: false,
+  numberOfReports: 17,
 };
 
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+
 console.log(teacher3);
-console.log('MAIN TS IS RUNNING');
+console.log(director1);
